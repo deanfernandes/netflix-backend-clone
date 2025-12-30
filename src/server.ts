a4fbac25c280c433d9d30ebb5c120261a28b6deb
dean <deanfernandes18@gmail.com>
2025-12-30 14:13:57 +0000

    fix apollo server ide in prod for demo purposes

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import {
   WatchlistFilmResolver,
   WatchlistSeriesResolver,
 } from "./gql/resolvers/Watchlist.js";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 
 export function createApolloServer() {
   const __dirname = process.cwd();
@@ -35,5 +36,10 @@ export function createApolloServer() {
     WatchlistSeries: WatchlistSeriesResolver,
   };
 
-  return new ApolloServer({ typeDefs, resolvers, introspection: true });
+  return new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
 }
